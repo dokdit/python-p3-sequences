deaae77afa14f98ab8a110dfc61bb8120382048c
Akuei <akueidok911@gmail.com>
2023-07-14 16:01:12 -0400

    Lab completed

--- lib/sequences.py
+++ lib/sequences.py
@@ -1,4 +1,13 @@
 #!/usr/bin/env python3
 
 def print_fibonacci(length):
-    pass
+    sequence = [0, 1]  # Initialize the sequence with the first two numbers
+    while len(sequence) < length:
+        next_number = sequence[-1] + sequence[-2]  # Calculate the next number
+        sequence.append(next_number)  # Add the next number to the sequence
+    if length == 0:
+        print([])
+    elif length == 1:
+        print([0])
+    else:
+        print(sequence)
